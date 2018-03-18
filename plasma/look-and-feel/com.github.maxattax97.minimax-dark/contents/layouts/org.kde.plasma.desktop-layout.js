9d1835e92db72cad0f66bc21bce26e9e32359c8e
Max O'Cull <maxattax97@gmail.com>
2018-03-17 22:01:48 -0400

    Add initial Minimax

---
+++ plasma/look-and-feel/com.github.maxattax97.minimax-dark/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,106 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        },
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "2",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true"
+                        },
+                        "/Configuration/Shortcuts": {
+                            "global": "Alt+F1"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.taskmanager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 120,
+            "minimumLength": 120,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
